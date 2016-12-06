7d12ed6539b28e908094960c63de5c4d45842f61
v-crispadvice <oleg.shadrunov@crispadvice.com>
2016-12-06 23:53:30 +0200

    updated to run as admin

--- Dynamics365/Operations/provisionAdminUser.ps1
+++ Dynamics365/Operations/provisionAdminUser.ps1
@@ -1,8 +1,13 @@
-﻿$CurUser = $env:userdomain + "\" + $env:username
-$AdObj = New-Object System.Security.Principal.NTAccount($CurUser)
-$strSID = $AdObj.Translate([System.Security.Principal.SecurityIdentifier])
-$sqlCmd = "UPDATE .dbo.[USERINFO] SET [SID]='{0}' WHERE [ID]='Admin'" -f $strSID.Value
+﻿If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{   
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    Break
+}
 
-Invoke-sqlcmd -querytimeout ([int]::MaxValue) -ServerInstance "localhost" -Database "DynamicsAX" -Query $sqlCmd
+$strUser = "{0}\{1}" -f $env:userdomain, $env:username
+$objUser = New-Object System.Security.Principal.NTAccount($strUser)
+$strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
+$strSQL = "UPDATE .dbo.[USERINFO] SET [SID]='{0}' WHERE [ID]='Admin'" -f $strSID
 
-
+Invoke-sqlcmd -querytimeout ([int]::MaxValue) -ServerInstance "localhost" -Database "DynamicsAX" -Query $strSQL
