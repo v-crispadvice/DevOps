2c7e166d9b19dc94c5adf257d587a4505287fc91
v-crispadvice <oleg.shadrunov@crispadvice.com>
2016-12-06 23:09:13 +0200

    added user provisioning

---
+++ Dynamics365/Operations/provisionAdminUser.ps1
@@ -0,0 +1,8 @@
+﻿$CurUser = $env:userdomain + "\" + $env:username
+$AdObj = New-Object System.Security.Principal.NTAccount($CurUser)
+$strSID = $AdObj.Translate([System.Security.Principal.SecurityIdentifier])
+$sqlCmd = "UPDATE .dbo.[USERINFO] SET [SID]='{0}' WHERE [ID]='Admin'" -f $strSID.Value
+
+Invoke-sqlcmd -querytimeout ([int]::MaxValue) -ServerInstance "localhost" -Database "DynamicsAX" -Query $sqlCmd
+
+
